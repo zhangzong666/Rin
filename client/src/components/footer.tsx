01a1b48cb4959f6a6883acd6105c51d3985345e4
zhangzong666 <109717018+zhangzong666@users.noreply.github.com>
2026-08-09 18:48:05 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -102,7 +102,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
+                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://blog.4km.cc.cd/about" target="_blank">DBD-blog</a>
                     </span>
                     {config.getBoolean('rss') && <>
                         <Spliter />
